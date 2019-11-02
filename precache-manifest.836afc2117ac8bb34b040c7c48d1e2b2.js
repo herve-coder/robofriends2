8b7d615bc09140288cea9f4319401ae91468fa85
TRAORE Hervé <htraore1976@gmail.com>
2019-11-02 18:32:16 +0000

    Updates

---
+++ precache-manifest.836afc2117ac8bb34b040c7c48d1e2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810147708248a85577f6a41edd35956b",
+    "url": "/robofriends2/index.html"
+  },
+  {
+    "revision": "a311acb686e8838a461d",
+    "url": "/robofriends2/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "feb8b317da3f6372e88d",
+    "url": "/robofriends2/static/css/main.77ecc3ae.chunk.css"
+  },
+  {
+    "revision": "a311acb686e8838a461d",
+    "url": "/robofriends2/static/js/2.8d3ddd93.chunk.js"
+  },
+  {
+    "revision": "feb8b317da3f6372e88d",
+    "url": "/robofriends2/static/js/main.79b26b90.chunk.js"
+  },
+  {
+    "revision": "374af767465f1c466dd2",
+    "url": "/robofriends2/static/js/runtime-main.d2c62e7a.js"
+  }
+]);
